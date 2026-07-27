72f39a9597e49dd8ab70e0fa6d21cfda875b26e7
HJK9810 <ruby45342@gmail.com>
2026-07-27 21:46:44 +0900

    solve: 2026-07-27 SWEA medium

--- solutions/HJK9810/2026-07-27/medium/1206.py
+++ solutions/HJK9810/2026-07-27/medium/1206.py
@@ -4,13 +4,14 @@ def algorithm():
     count = 0
 
     for index in range(2, N - 2):
-        max_height = max(buildings[index -2], buildings[index - 1], buildings[index + 1], buildings[index + 2])
-        if buildings[index] > max_height:
-            count += (buildings[index] - max_height)
+        now_height = buildings[index]
+
+        if now_height > buildings[index - 2] and now_height > buildings[index - 1] and now_height > buildings[index + 1] and now_height > buildings[index + 2]:
+            max_height = max(max(buildings[index - 2], buildings[index - 1]), max(buildings[index + 1], buildings[index + 2]))
+            count += (now_height - max_height)
 
     return count
 
-
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, 11):
-    print(f"#{test_case} ${algorithm()}")
+    print(f"#{test_case} {algorithm()}")
