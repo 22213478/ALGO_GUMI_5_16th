4a50f13e7ef2a44c4c083c85e585b949990bcbc9
HJK9810 <ruby45342@gmail.com>
2026-07-27 23:18:36 +0900

    solve: 2026-07-27 SWEA high

--- solutions/HJK9810/2026-07-27/high/1247.py
+++ solutions/HJK9810/2026-07-27/high/1247.py
@@ -3,19 +3,6 @@ def calc_dist(first, second):
     x2, y2 = second
     return abs(x1 - x2) + abs(y1 - y2)
 
-def find_dist(pos, before_idx, count, visited, distance, min_dist):
-    if count == len(pos):
-        return before_idx, distance if min_dist > distance else min_dist
-
-    for idx in range(len(pos)):
-        if visited[idx]: continue
-
-        visited[idx] = True
-        find_dist(pos, idx, count + 1, visited, distance + calc_dist(pos[before_idx], pos[idx]), min_dist)
-        visited[idx] = False
-
-    return -1, min_dist
-
 def algorithm():
     SIZE = int(input())
     input_post = list(map(int, input().split()))
@@ -27,20 +14,27 @@ def algorithm():
         cus_pos.append(input_post[idx * 2:idx * 2 + 2])
 
     dist = float('inf')
+    visited = [False] * SIZE
+
+    def find_dist(current_idx, count, now_dist):
+        nonlocal dist
+
+        if now_dist >= dist: return
+        if count == SIZE:
+            dist = min(dist, now_dist + calc_dist(cus_pos[current_idx], home_pos))
+            return
+
+        for idx in range(SIZE):
+            if visited[idx]: continue
+
+            visited[idx] = True
+            find_dist(idx, count + 1, now_dist + calc_dist(cus_pos[current_idx], cus_pos[idx]))
+            visited[idx] = False
 
     for idx in range(SIZE):
-        s_x, s_y = office_pos
-        f_x, f_y = cus_pos[idx]
-        visited = [False] * SIZE
         visited[idx] = True
-
-        last_idx, distance = find_dist(cus_pos, idx, 1, visited, abs(f_x - s_x) + abs(f_y - s_y), dist)
-
-        if last_idx == -1: continue
-        e_x, e_y = cus_pos[last_idx]
-        h_x, h_y = home_pos
-
-        dist = min(dist, abs(e_x - h_x) + abs(e_y - h_y) + distance)
+        find_dist(idx, 1, calc_dist(cus_pos[idx], office_pos))
+        visited[idx] = False
 
     return dist
 
